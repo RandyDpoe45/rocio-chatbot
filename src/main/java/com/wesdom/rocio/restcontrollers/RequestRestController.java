a59b00316949ac0aa53c695e7127389b4634045d
RandyDpoe45 <randydarrell45@hotmail.com>
2020-11-13 18:33:50 -0500

    query api

--- src/main/java/com/wesdom/rocio/restcontrollers/RequestRestController.java
+++ src/main/java/com/wesdom/rocio/restcontrollers/RequestRestController.java
@@ -79,7 +79,7 @@ public class RequestRestController {
                 setBlocked_input(Boolean.TRUE).setChannel(request.getString("channel")).setModule_id(request.getString("module_id")).
                 setSuggested_replies(suggestedRep).setMessage("Estos son tus problemas");
         }catch(Exception e){
-            
+            e.printStackTrace();
             return new WebhookDto();
         }
     }
