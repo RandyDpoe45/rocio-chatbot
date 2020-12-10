8e33706419149412b0cf5e030cf1098a658844d7
RandyDpoe45 <randydarrell45@hotmail.com>
2020-12-09 21:26:38 -0500

    bug in diagnosisGroup solved

--- src/main/java/com/wesdom/rocio/servicesimpl/DiagnosisGroupServiceImpl.java
+++ src/main/java/com/wesdom/rocio/servicesimpl/DiagnosisGroupServiceImpl.java
@@ -5,7 +5,10 @@
  */
 package com.wesdom.rocio.servicesimpl;
 
+import com.wesdom.rocio.database.repositories.ApprenticeRepository;
 import com.wesdom.rocio.database.repositories.GroupRepository;
+import com.wesdom.rocio.database.repositories.KnowledgeAreaRepository;
+import com.wesdom.rocio.model.Apprentice;
 import com.wesdom.rocio.model.DiagnosisGroup;
 import com.wesdom.rocio.model.KnowledgeArea;
 import com.wesdom.rocio.services.DiagnosisGroupService;
@@ -28,12 +31,23 @@ public class DiagnosisGroupServiceImpl implements DiagnosisGroupService {
     @Autowired
     private GroupRepository groupRepository;
 
+    @Autowired
+    private ApprenticeRepository apprenticeRepository;
+
+    @Autowired
+    private KnowledgeAreaRepository knowledgeAreaRepository;
+
     @PersistenceContext
     private EntityManager em;
 
     @Override
     @Transactional
     public DiagnosisGroup create(DiagnosisGroup group) {
+        List<Long> knowledgeAreaIds = group.getKnowledgeAreas().stream().map(x -> x.getId()).collect(Collectors.toList());
+        List<Long> apprendiceIds = group.getApprentices().stream().map(x -> x.getId()).collect(Collectors.toList());
+        List<Apprentice> apprentices = apprenticeRepository.getAllById(apprendiceIds);
+        List<KnowledgeArea> knowledgeAreas = knowledgeAreaRepository.getAllById(knowledgeAreaIds);
+        group.setApprentices(apprentices).setKnowledgeAreas(knowledgeAreas);
         DiagnosisGroup g = groupRepository.create(group);
         em.refresh(g);
         return g;
@@ -42,6 +56,11 @@ public class DiagnosisGroupServiceImpl implements DiagnosisGroupService {
     @Override
     @Transactional
     public DiagnosisGroup update(Long id, DiagnosisGroup group) {
+        List<Long> knowledgeAreaIds = group.getKnowledgeAreas().stream().map(x -> x.getId()).collect(Collectors.toList());
+        List<Long> apprendiceIds = group.getApprentices().stream().map(x -> x.getId()).collect(Collectors.toList());
+        List<Apprentice> apprentices = apprenticeRepository.getAllById(apprendiceIds);
+        List<KnowledgeArea> knowledgeAreas = knowledgeAreaRepository.getAllById(knowledgeAreaIds);
+        group.setApprentices(apprentices).setKnowledgeAreas(knowledgeAreas);
         DiagnosisGroup g = groupRepository.update(id,group);
         em.refresh(g);
         return g;
